f4fc3fd15a46498d9146471320820fd72397484d
nunn3152 <annunn@csumb.edu>
2015-02-11 19:02:29 +0000

    fu

--- index.php
+++ index.php
@@ -9,7 +9,52 @@
  */
 
 
+$suits = array(
+    "0" => "clubs",
+    "1" => "diamonds",
+    "2" => "hearts",
+    "3" => "spades",
+);
 
 
 
 ?>
+
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Lab 2: Multiplying Playing Cards</title>
+        <meta charset="utf-8" />
+        
+        <style>
+        .symbols {
+            font-size: 6em;
+        }
+        </style>
+    </head>
+    
+    <body>
+
+
+        <!--  -->
+
+        <?= drawCards() ?>  
+        <form action="index.php" method="get">
+            <input type="submit" value="Play Again">
+        </form>
+        
+        <footer>&copy; Created by: Team 7</footer>
+    </body>
+
+    
+    
+</html>
+
+
+
+
