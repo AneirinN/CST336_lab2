0a138ae8b9bb252f68b05f89768d7af5e93b2227
boen3764 <mboensel@csumb.edu>
2015-02-12 06:52:04 +0000

    finished all the random selection logic and the determination of winner/tie

--- includes/matt.php
+++ includes/matt.php
@@ -1,5 +1,7 @@
 <?php
 
+$pointsOff = array(); //array to track deviation from 42
+  
 //first player array
 
 $matt = array();
@@ -24,6 +26,19 @@ for($i = 0; $i < 5; $i++)
     
 }
 
+$mattDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($mattVal < 42)
+{
+    $mattDiff = 42 - $mattVal;
+}    
+
+else if($mattVal > 42)
+{
+    $mattDiff = $mattVal - 42;    
+}
+
+array_push($pointsOff, $mattDiff); //push deviation value onto array
 
 //second player
 
@@ -49,6 +64,20 @@ for($i = 0; $i < 5; $i++)
     
 }
 
+$aneiDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($aneiVal < 42)
+{
+    $aneiDiff = 42 - $aneiVal;
+}    
+
+else if($aneiVal > 42)
+{
+    $aneiDiff = $aneiVal - 42;    
+}
+
+array_push($pointsOff, $aneiDiff); //push deviation value onto array
+
 //third player
 
 $zach = array();
@@ -73,6 +102,21 @@ for($i = 0; $i < 5; $i++)
     
 }
 
+$zachDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($zachVal < 42)
+{
+    $zachDiff = 42 - $zachVal;
+}    
+
+else if($zachVal > 42)
+{
+    $zachDiff = $zachVal - 42;    
+}
+
+array_push($pointsOff, $zachDiff); //push deviation value onto array
+
+
 //fourth player
 
 $hal = array();
@@ -95,6 +139,65 @@ for($i = 0; $i < 5; $i++)
     
     $halVal += $file['0']; //take first part of cut _.png, getting card value and incrementing hand value
     
+}
+
+$halDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($halVal < 42)
+{
+    $halDiff = 42 - $halVal;
+}    
+
+else if($halVal > 42)
+{
+    $halDiff = $halVal - 42;    
+}
+
+array_push($pointsOff, $halDiff); //push deviation value onto array
+
+
+$tie = false; //if find a tie, just set to true, will use to change message to "It's a Tie" using if($tie)
+
+$min_key = -1; //min_key value of 0 is matt, 1 is anei, etc. not the most elegant solution but I'm lazy
+
+$min_value = 10000; //arbitrarily large number to insure that first iteration of loop compares favorably as a min value
+
+foreach($pointsOff as $key => $value)  
+{
+    if($value == $min_value) //have to do this check first otherwise it is autotrue from first iteration
+    {
+         $tie = true;
+    }   
+    
+    if($value < $min_value)
+    {
+        $min_value = $value;
+        $min_key = $key;
+    }
+}
+
+if($tie) //check if there is a tie
+{
+    echo "It's a Tie";
+}
+
+else //if not, find out who won
+{
+    switch($min_key)
+    {
+        case 0:
+            echo "Matt wins!";
+            break;
+        case 1:
+            echo "Aneirin wins!";
+            break;
+        case 2:
+            echo "Zach wins!";
+            break;
+        case 3:
+            echo "Hal wins!";
+            break;
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -64,6 +64,7 @@ $suits = array(
     "51" => "img/spades/13.png",
 );
 
+$pointsOff = array(); //array to track deviation from 42
   
 //first player array
 
@@ -89,6 +90,19 @@ for($i = 0; $i < 5; $i++)
     
 }
 
+$mattDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($mattVal < 42)
+{
+    $mattDiff = 42 - $mattVal;
+}    
+
+else if($mattVal > 42)
+{
+    $mattDiff = $mattVal - 42;    
+}
+
+array_push($pointsOff, $mattDiff); //push deviation value onto array
 
 //second player
 
@@ -114,6 +128,20 @@ for($i = 0; $i < 5; $i++)
     
 }
 
+$aneiDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($aneiVal < 42)
+{
+    $aneiDiff = 42 - $aneiVal;
+}    
+
+else if($aneiVal > 42)
+{
+    $aneiDiff = $aneiVal - 42;    
+}
+
+array_push($pointsOff, $aneiDiff); //push deviation value onto array
+
 //third player
 
 $zach = array();
@@ -137,6 +165,21 @@ for($i = 0; $i < 5; $i++)
     $zachVal += $file['0']; //take first part of cut _.png, getting card value and incrementing hand value
     
 }
+
+$zachDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($zachVal < 42)
+{
+    $zachDiff = 42 - $zachVal;
+}    
+
+else if($zachVal > 42)
+{
+    $zachDiff = $zachVal - 42;    
+}
+
+array_push($pointsOff, $zachDiff); //push deviation value onto array
+
 
 //fourth player
 
@@ -162,12 +205,75 @@ for($i = 0; $i < 5; $i++)
     
 }
 
-//TODO: display cards, values, pictures. determine ties/victors
+$halDiff = 0; //Difference defaults to zero, if is different than 42 (greater or less than), if statements catch that
+
+if($halVal < 42)
+{
+    $halDiff = 42 - $halVal;
+}    
+
+else if($halVal > 42)
+{
+    $halDiff = $halVal - 42;    
+}
+
+array_push($pointsOff, $halDiff); //push deviation value onto array
+
+
+$tie = false; //if find a tie, just set to true, will use to change message to "It's a Tie" using if($tie)
+
+$min_key = -1; //min_key value of 0 is matt, 1 is anei, etc. not the most elegant solution but I'm lazy
+
+$min_value = 10000; //arbitrarily large number to insure that first iteration of loop compares favorably as a min value
+
+foreach($pointsOff as $key => $value)  
+{
+    if($value == $min_value) //have to do this check first otherwise it is autotrue from first iteration
+    {
+         $tie = true;
+    }   
+    
+    if($value < $min_value)
+    {
+        $min_value = $value;
+        $min_key = $key;
+    }
+}
+
+if($tie) //check if there is a tie
+{
+    echo "It's a Tie";
+}
+
+else //if not, find out who won
+{
+    switch($min_key)
+    {
+        case 0:
+            echo "Matt wins!";
+            break;
+        case 1:
+            echo "Aneirin wins!";
+            break;
+        case 2:
+            echo "Zach wins!";
+            break;
+        case 3:
+            echo "Hal wins!";
+            break;
+    }
+}
+
+
+//TODO: display cards, values, pictures
 /*
- * Pseudocode: loop to find highest value using variables above, then compare to make sure no ties
- * set message to ___ wins or ____ and ____ tie as necessary
  * some CSS nonsense w/ html to get pretty picture
  * Zach/Aneirin can you take care of this?
+ * relevant variables are $matt etc for the hand arrays, $mattVal etc for value of hands,
+ * $tie as a bool to control what we output, $min_key which tells us who won (if no tie)
+ * I've already done a loose implementation of the winner message, it just needs to be adapted into a html/css friendly one that
+ * also displays the images. All the logic is complete.
+ * Code is tested (not extensively yet though), and functions as intended. Up to you guys now.
  * -Matt
  */
     
